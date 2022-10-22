af9c44ad6243381b3ce5dba8ee7b34154c73b2be
Taby <forwardpivot@proton.me>
2022-10-22 16:26:23 -0500

    Made the circle bounce off wall

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 let canvas = document.querySelector('#canvas');
+let ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+ctx.beginPath();
+ctx.arc(500,500,30,0,Math.PI * 2);
+ctx.strokeStyle = 'blue';
+ctx.stroke();
+
+let x = 500;
+let dx = 1;
+function animate() {
+        ctx.clearRect(0,0,innerWidth,innerHeight);
+        ctx.beginPath();
+        ctx.arc(x,500,30,0,Math.PI * 2);
+        ctx.strokeStyle = 'blue';
+        ctx.fill();
+        ctx.stroke();
+
+        if (x > innerWidth) 
+            dx = -1; 
+
+        x += dx;
+    requestAnimationFrame(animate);
+}
+
+animate();
